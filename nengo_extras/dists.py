e691ebf45dce6b6521f036b6ca59c9498d690849
Eric Hunsberger <erichuns@gmail.com>
2016-10-27 14:26:26 -0400

    Added Mixture distribution

--- nengo_extras/dists.py
+++ nengo_extras/dists.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from nengo.dists import Distribution
-from nengo.params import NdarrayParam
+from nengo.params import NdarrayParam, TupleParam
 
 
 class MultivariateGaussian(Distribution):
@@ -20,3 +20,40 @@ class MultivariateGaussian(Distribution):
     def sample(self, n, d=None, rng=np.random):
         assert d is None or d == self.d
         return rng.multivariate_normal(self.mean, self.cov, size=n)
+
+
+class Mixture(Distribution):
+    distributions = TupleParam('distributions')
+    p = NdarrayParam('p', shape='*', optional=True)
+
+    def __init__(self, distributions, p=None):
+        super(Mixture, self).__init__()
+
+        self.distributions = distributions
+        if not all(isinstance(d, Distribution) for d in self.distributions):
+            raise ValueError(
+                "All elements in `distributions` must be Distributions")
+
+        if p is not None:
+            p = np.array(p)
+            if p.ndim != 1 or p.size != len(self.distributions):
+                raise ValueError(
+                    "`p` must be a vector with one element per distribution")
+            if (p < 0).any():
+                raise ValueError("`p` must be all non-negative")
+            p /= p.sum()
+        self.p = p
+
+    def sample(self, n, d=None, rng=np.random):
+        dd = 1 if d is None else d
+        samples = np.zeros((n, dd))
+
+        nd = len(self.distributions)
+        i = (rng.randint(nd, size=n) if self.p is None else
+             rng.choice(nd, p=self.p, size=n))
+        c = np.bincount(i, minlength=nd)
+
+        for k in c.nonzero()[0]:
+            samples[i == k] = self.distributions[k].sample(c[k], d=dd, rng=rng)
+
+        return samples[:, 0] if d is None else samples
--- nengo_extras/tests/test_dists.py
+++ nengo_extras/tests/test_dists.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from nengo_extras.dists import MultivariateGaussian
+from nengo.dists import Gaussian, Uniform
+from nengo_extras.dists import Mixture, MultivariateGaussian
 
 
 def test_multivariate_gaussian(plt, rng):
@@ -10,3 +11,31 @@ def test_multivariate_gaussian(plt, rng):
     pts = dist.sample(n, rng=rng)
 
     plt.hist2d(pts[:, 0], pts[:, 1], bins=np.arange(-5, 5, 0.2))
+
+
+def test_mixture_1d(plt, rng):
+    n = 100000
+    bins = np.arange(-5, 5, 0.1)
+
+    dists = [Uniform(-0.5, 0.5), Gaussian(-1, 1), Gaussian(2, 1)]
+    mdists = [Mixture(dists),
+              Mixture(dists, p=[0.7, 0.15, 0.15])]
+
+    for k, dist in enumerate(mdists):
+        plt.subplot(len(mdists), 1, k+1)
+        pts = dist.sample(n, rng=rng)
+        plt.hist(pts, bins=bins)
+
+
+def test_mixture_2d(plt, rng):
+    n = 200000
+
+    dists = [MultivariateGaussian((-2, -1), (0.25, 4)),
+             MultivariateGaussian((1, 1), (1, 1))]
+    mdists = [Mixture(dists),
+              Mixture(dists, p=[0.7, 0.3])]
+
+    for k, dist in enumerate(mdists):
+        plt.subplot(len(mdists), 1, k+1)
+        pts = dist.sample(n, d=2, rng=rng)
+        plt.hist2d(pts[:, 0], pts[:, 1], bins=np.arange(-5, 5, 0.2))
