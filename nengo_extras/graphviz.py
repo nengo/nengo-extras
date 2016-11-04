069fef462d9afc8e073218e44b3203cdb70a8601
Terry Stewart <tcstewar@uwaterloo.ca>
2016-11-04 11:34:23 -0400

    Add functions to generate graphviz diagrams
    Migrated from Nengo.

---
+++ nengo_extras/graphviz.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def net_diagram(net):
+    """Create a .dot file showing nodes, ensmbles, and connections.
+
+    This can be useful for debugging and testing builders that manipulate
+    the model graph before construction.
+
+    Parameters
+    ----------
+    net : Network
+        A network from which objects and connections will be extracted.
+
+    Returns
+    -------
+    text : string
+        Text content of the desired .dot file.
+    """
+    objs = net.all_nodes + net.all_ensembles
+    return obj_conn_diagram(objs, net.all_connections)
+
+
+def obj_conn_diagram(objs, connections):
+    """Create a .dot file showing nodes, ensmbles, and connections.
+
+    This can be useful for debugging and testing builders that manipulate
+    the model graph before construction.
+
+    Parameters
+    ----------
+    objs : list of Nodes and Ensembles
+        All the nodes and ensembles in the model.
+    connections : list of Connections
+        All the connections in the model.
+
+    Returns
+    -------
+    text : string
+        Text content of the desired .dot file.
+    """
+    text = []
+    text.append('digraph G {')
+    for obj in objs:
+        text.append('  "%d" [label="%s"];' % (id(obj), obj.label))
+
+    def label(transform):
+        # determine the label for a connection based on its transform
+        transform = np.asarray(transform)
+        if len(transform.shape) == 0:
+            return ''
+        return '%dx%d' % transform.shape
+
+    for c in connections:
+        text.append('  "%d" -> "%d" [label="%s"];' % (
+            id(c.pre_obj), id(c.post_obj), label(c.transform)))
+    text.append('}')
+    return '\n'.join(text)
---
+++ nengo_extras/tests/test_graphviz.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+import nengo
+from nengo.utils.builder import remove_passthrough_nodes
+
+from nengo_extras.graphviz import net_diagram, obj_conn_diagram
+
+
+def make_net():
+    with nengo.Network() as model:
+        D = 3
+        input = nengo.Node([1] * D, label='input')
+        a = nengo.networks.EnsembleArray(50, D, label='a')
+        b = nengo.networks.EnsembleArray(50, D, label='b')
+        output = nengo.Node(None, size_in=D, label='output')
+
+        nengo.Connection(input, a.input, synapse=0.01)
+        nengo.Connection(a.output, b.input, synapse=0.01)
+        nengo.Connection(b.output, b.input, synapse=0.01, transform=0.9)
+        nengo.Connection(a.output, a.input, synapse=0.01,
+                         transform=np.ones((D, D)))
+        nengo.Connection(b.output, output, synapse=0.01)
+    return model
+
+
+def test_net_diagram():
+    """Constructing a .dot file for a network."""
+    dot = net_diagram(make_net())
+    assert len(dot.splitlines()) == 31
+
+
+def test_obj_conn_diagram():
+    """Constructing a .dot file for a set of objects and connections."""
+    model = make_net()
+    objs = model.all_nodes + model.all_ensembles
+    conns = model.all_connections
+
+    dot = obj_conn_diagram(objs, conns)
+    assert len(dot.splitlines()) == 31
+
+    dot = obj_conn_diagram(*remove_passthrough_nodes(objs, conns))
+    assert len(dot.splitlines()) == 27
