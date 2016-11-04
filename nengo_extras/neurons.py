9a57c296f2382e24ab5d9ca3a2a7546ea5a5ca6d
Eric Hunsberger <erichuns@gmail.com>
2016-11-04 13:42:23 -0400

    Add functions to estimate firing rates.
    Migrated from Nengo.

--- nengo_extras/neurons.py
+++ nengo_extras/neurons.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 import nengo
+import nengo.utils.numpy as npext
+from nengo.exceptions import ValidationError
 from nengo.params import NumberParam
 
 
@@ -120,3 +122,132 @@ class FastLIF(nengo.neurons.LIF):
         # set spiking neurons' voltages to zero, and ref. time to tau_ref
         voltage[spiked > 0] = 0
         refractory_time[spiked > 0] = self.tau_ref + spiketime
+
+
+def spikes2events(t, spikes):
+    """Return an event-based representation of spikes (i.e. spike times)"""
+    spikes = npext.array(spikes, copy=False, min_dims=2)
+    if spikes.ndim > 2:
+        raise ValidationError("Cannot handle %d-dimensional arrays"
+                              % spikes.ndim, attr='spikes')
+    if spikes.shape[-1] != len(t):
+        raise ValidationError("Last dimension of 'spikes' must equal 'len(t)'",
+                              attr='spikes')
+
+    # find nonzero elements (spikes) in each row, and translate to times
+    return [t[spike != 0] for spike in spikes]
+
+
+def _rates_isi_events(t, events, midpoint, interp):
+    import scipy.interpolate
+
+    if len(events) == 0:
+        return np.zeros_like(t)
+
+    isis = np.diff(events)
+
+    rt = np.zeros(len(events) + (1 if midpoint else 2))
+    rt[1:-1] = 0.5*(events[:-1] + events[1:]) if midpoint else events
+    rt[0], rt[-1] = t[0], t[-1]
+
+    r = np.zeros_like(rt)
+    r[1:len(isis) + 1] = 1. / isis
+
+    f = scipy.interpolate.interp1d(rt, r, kind=interp, copy=False)
+    return f(t)
+
+
+def rates_isi(t, spikes, midpoint=False, interp='zero'):
+    """Estimate firing rates from spikes using ISIs.
+
+    Parameters
+    ----------
+    t : (M,) array_like
+        The times at which raw spike data (spikes) is defined.
+    spikes : (M, N) array_like
+        The raw spike data from N neurons.
+    midpoint : bool, optional
+        If true, place interpolation points at midpoints of ISIs. Otherwise,
+        the points are placed at the beginning of ISIs.
+    interp : string, optional
+        Interpolation type, passed to `scipy.interpolate.interp1d` as the
+        `kind` parameter.
+
+    Returns
+    -------
+    rates : (M, N) array_like
+        The estimated neuron firing rates.
+    """
+    spike_times = spikes2events(t, spikes.T)
+    rates = np.zeros(spikes.shape)
+    for i, st in enumerate(spike_times):
+        rates[:, i] = _rates_isi_events(t, st, midpoint, interp)
+
+    return rates
+
+
+def lowpass_filter(x, tau, kind='expon'):
+    nt = x.shape[-1]
+
+    if kind == 'expon':
+        t = np.arange(0, 5 * tau)
+        kern = np.exp(-t / tau) / tau
+        delay = tau
+    elif kind == 'gauss':
+        std = tau / 2.
+        t = np.arange(-4 * std, 4 * std)
+        kern = np.exp(-0.5 * (t / std)**2) / np.sqrt(2 * np.pi * std**2)
+        delay = 4 * std
+    elif kind == 'alpha':
+        alpha = 1. / tau
+        t = np.arange(0, 5 * tau)
+        kern = alpha**2 * t * np.exp(-alpha * t)
+        delay = tau
+    else:
+        raise ValidationError("Unrecognized filter kind '%s'" % kind, 'kind')
+
+    delay = int(np.round(delay))
+    return np.array(
+        [np.convolve(kern, xx, mode='full')[delay:nt + delay] for xx in x])
+
+
+def rates_kernel(t, spikes, kind='gauss', tau=0.04):
+    """Estimate firing rates from spikes using a kernel.
+
+    Parameters
+    ----------
+    t : (M,) array_like
+        The times at which raw spike data (spikes) is defined.
+    spikes : (M, N) array_like
+        The raw spike data from N neurons.
+    kind : str {'expon', 'gauss', 'expogauss', 'alpha'}, optional
+        The type of kernel to use. 'expon' is an exponential kernel, 'gauss' is
+        a Gaussian (normal) kernel, 'expogauss' is an exponential followed by
+        a Gaussian, and 'alpha' is an alpha function kernel.
+    tau : float
+        The time constant for the kernel. The optimal value will depend on the
+        firing rate of the neurons, with a longer tau preferred for lower
+        firing rates. The default value of 0.04 works well across a wide range
+        of firing rates.
+    """
+    spikes = spikes.T
+    spikes = npext.array(spikes, copy=False, min_dims=2)
+    if spikes.ndim > 2:
+        raise ValidationError("Cannot handle %d-dimensional arrays"
+                              % spikes.ndim, attr='spikes')
+    if spikes.shape[-1] != len(t):
+        raise ValidationError("Last dimension of 'spikes' must equal 'len(t)'",
+                              attr='spikes')
+
+    n, nt = spikes.shape
+    dt = t[1] - t[0]
+
+    tau_i = tau / dt
+    kind = kind.lower()
+    if kind == 'expogauss':
+        rates = lowpass_filter(spikes, tau_i, kind='expon')
+        rates = lowpass_filter(rates, tau_i / 4, kind='gauss')
+    else:
+        rates = lowpass_filter(spikes, tau_i, kind=kind)
+
+    return rates.T
