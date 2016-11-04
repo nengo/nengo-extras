132149c99c6478146629c4a27e9bb5c831c683a9
Youssef Zaky <youssefzaky@gmail.com>
2016-11-04 13:58:04 -0400

    Add `probe_all` function.
    Migrated from Nengo.

--- nengo_extras/__init__.py
+++ nengo_extras/__init__.py
@@ -2,4 +2,4 @@ from .version import version as __version__
 
 from .convnet import Conv2d, Pool2d
 from .neurons import FastLIF, SoftLIFRate
-from . import data, dists, graphviz, gui, networks, neurons, vision
+from . import data, dists, graphviz, gui, networks, neurons, probe, vision
