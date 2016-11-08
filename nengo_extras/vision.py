d70109016371fb15f09c512986606c75e550ec16
Eric Hunsberger <erichuns@gmail.com>
2016-11-08 16:38:44 -0500

    Added vision encoder finding methods

--- nengo_extras/vision.py
+++ nengo_extras/vision.py
@@ -3,6 +3,7 @@ import numpy as np
 from nengo.dists import Choice, Uniform, DistributionParam
 from nengo.params import FrozenObject, TupleParam
 from nengo.utils.compat import range
+import nengo.utils.numpy as npext
 
 
 class Gabor(FrozenObject):
@@ -97,3 +98,127 @@ class Mask(FrozenObject):
             output[k, :, i[k]:i[k]+shape[0], j[k]:j[k]+shape[1]] = filters[k]
 
         return output.reshape(n, -1) if flatten else output
+
+
+def ciw_encoders(n_encoders, trainX, trainY, rng=np.random,
+                 normalize_data=True, normalize_encoders=True):
+    """Computed Input Weights (CIW) method for encoders from data [1]_.
+
+    Parameters
+    ==========
+    n_encoders : int
+        Number of encoders to generate.
+    trainX : (n_samples, n_dimensions) array-like
+        Training features.
+    trainY : (n_samples,) array-like
+        Training labels.
+
+    Returns
+    =======
+    encoders : (n_encoders, n_dimensions) array
+        Generated encoders.
+
+    References
+    ==========
+    .. [1] McDonnell, M. D., Tissera, M. D., Vladusich, T., Van Schaik, A.,
+       Tapson, J., & Schwenker, F. (2015). Fast, simple and accurate
+       handwritten digit classification by training shallow neural network
+       classifiers with the "Extreme learning machine" algorithm. PLoS ONE,
+       10(8), 1-20. doi:10.1371/journal.pone.0134254
+    """
+    assert trainX.shape[0] == trainY.size
+    trainX = trainX.reshape(trainX.shape[0], -1)
+    trainY = trainY.ravel()
+    classes = np.unique(trainY)
+
+    assert n_encoders % len(classes) == 0
+    n_enc_per_class = n_encoders / len(classes)
+
+    # normalize
+    if normalize_data:
+        trainX = (trainX - trainX.mean()) / trainX.std()
+        # trainX = (trainX - trainX.mean(axis=0)) / trainX.std()
+        # trainX = (trainX - trainX.mean(axis=0)) / (trainX.std(axis=0) + 1e-8)
+
+    # generate
+    encoders = []
+    for label in classes:
+        X = trainX[trainY == label]
+        plusminus = rng.choice([-1, 1], size=(X.shape[0], n_enc_per_class))
+        samples = np.dot(plusminus.T, X)
+        encoders.append(samples)
+
+    encoders = np.vstack(encoders)
+    if normalize_encoders:
+        encoders /= npext.norm(encoders, axis=1, keepdims=True)
+
+    return encoders
+
+
+def cd_encoders_biases(n_encoders, trainX, trainY, rng=np.random, mask=None,
+                       norm_min=0.05, norm_tries=10):
+    """Constrained difference (CD) method for encoders from data [1]_.
+
+    Parameters
+    ==========
+    n_encoders : int
+        Number of encoders to generate.
+    trainX : (n_samples, n_dimensions) array-like
+        Training features.
+    trainY : (n_samples,) array-like
+        Training labels.
+
+    Returns
+    =======
+    encoders : (n_encoders, n_dimensions) array
+        Generated encoders.
+    biases : (n_encoders,) array
+        Generated biases. These are biases assuming `f = G[E * X + b]`,
+        and are therefore more like Nengo's `intercepts`.
+
+    References
+    ==========
+    .. [1] McDonnell, M. D., Tissera, M. D., Vladusich, T., Van Schaik, A.,
+       Tapson, J., & Schwenker, F. (2015). Fast, simple and accurate
+       handwritten digit classification by training shallow neural network
+       classifiers with the "Extreme learning machine" algorithm. PLoS ONE,
+       10(8), 1-20. doi:10.1371/journal.pone.0134254
+    """
+    assert trainX.shape[0] == trainY.size
+    trainX = trainX.reshape(trainX.shape[0], -1)
+    trainY = trainY.ravel()
+    d = trainX.shape[1]
+    classes = np.unique(trainY)
+    assert mask is None or mask.shape == (n_encoders, d)
+
+    inds = [(trainY == label).nonzero()[0] for label in classes]
+    train_norm = npext.norm(trainX, axis=1).mean()
+
+    encoders = np.zeros((n_encoders, d))
+    biases = np.zeros(n_encoders)
+    for k in range(n_encoders):
+        for _ in range(norm_tries):
+            i, j = rng.choice(len(classes), size=2, replace=False)
+            a, b = trainX[rng.choice(inds[i])], trainX[rng.choice(inds[j])]
+            dab = a - b
+            if mask is not None:
+                dab *= mask[k]
+            ndab = npext.norm(dab)**2
+            if ndab >= norm_min * train_norm:
+                break
+        else:
+            raise ValueError("Cannot find valid encoder")
+
+        encoders[k] = (2. / ndab) * dab
+        biases[k] = np.dot(a + b, dab) / ndab
+
+    return encoders, biases
+
+
+def percentile_biases(encoders, trainX, percentile=50):
+    """Pick biases such that neurons are active for a percentile of inputs.
+    """
+    trainX = trainX.reshape(trainX.shape[0], -1)
+    H = np.dot(trainX, encoders.T)
+    biases = np.percentile(H, percentile, axis=0)
+    return biases
