5f9656563c5563e77a72aa1ef84ff806f0f98094
Eric Hunsberger <erichuns@gmail.com>
2017-06-08 16:47:30 -0400

    Unpickle both utf8 and latin1 strings in Python3
    This allows Python2 pickle files to have strings encoded as either
    utf8 or ascii (latin1).
    
    The only disadvantage is that it uses the pure-python ``pickle``
    implementation, rather than the faster C-based ``_pickle``. In
    practice, though, this doesn't seem much slower.

--- nengo_extras/utils.py
+++ nengo_extras/utils.py
@@ -8,20 +8,27 @@ urlretrieve = urllib.urlretrieve if PY2 else urllib.request.urlretrieve
 
 if PY2:
     from cStringIO import StringIO  # noqa: F401
+
+    def pickle_load(file, *args, **kwargs):
+        return pickle.load(file, *args, **kwargs)
+
 else:
     from io import StringIO  # noqa: F401
 
     def cmp(a, b):
         return (a > b) - (a < b)
 
+    from pickle import _Unpickler
+    class _UnpicklerStringsafe(_Unpickler):
+        def _decode_string(self, value):
+            if self.encoding == "bytes":
+                return value
+            else:
+                try:
+                    return value.decode(self.encoding, self.errors)
+                except UnicodeDecodeError:
+                    return value.decode('latin1')
 
-def pickle_load(file, *args, **kwargs):
-    if not PY2:
-        kwargs.setdefault('encoding', 'latin1')
-    return pickle.load(file, *args, **kwargs)
-
-
-def pickle_load_bytes(file, *args, **kwargs):
-    if not PY2:
-        kwargs.setdefault('encoding', 'bytes')
-    return pickle.load(file, *args, **kwargs)
+    def pickle_load(file, *args, **kwargs):
+        kwargs.setdefault('encoding', 'utf8')
+        return _UnpicklerStringsafe(file, **kwargs).load()
