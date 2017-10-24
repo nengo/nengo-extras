45df592a7f11370cf6b3726aa44bc9f7a978e5c3
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-10-24 11:28:20 -0400

    fixup! squash: require nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,7 @@ try:
     import nengo_extras
     import guzzle_sphinx_theme
 except ImportError:
-    print("To build the documentation, "
-          "nbsphinx, nengo_extras, and guzzle_sphinx_theme "
+    print("To build the documentation, nengo_extras and guzzle_sphinx_theme "
           "must be installed in the current environment. Please install these "
           "and their requirements first. A virtualenv is recommended!")
     sys.exit(1)
